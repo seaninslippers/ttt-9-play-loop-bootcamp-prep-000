fa6f07f0073d7d7592d6fcd62801c9ad77b6d0b0
Sean Xiong <seanxionghe@gmail.com>
2020-04-22 22:46:00 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,21 @@ def turn(board)
 end
 
 # Define your play method below
+def play(board)
+  until board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+    turn(board)
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
